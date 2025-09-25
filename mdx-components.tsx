5fc171460ae7c7d77ec2a20c4e24916d43e7199f
ditinagrawal <ditin.agrawal05@gmail.com>
2025-09-25 12:19:37 +0530

    feat(templates): add templates [portfolio]

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,4 @@
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
@@ -6,6 +7,7 @@ import type { MDXComponents } from "mdx/types";
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    img: (props) => <ImageZoom {...props} />,
     ...TabsComponents,
     ...components,
   };
